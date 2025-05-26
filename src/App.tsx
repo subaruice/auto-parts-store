37bee5106ec0376a0c6d4392c4a2b77bf958013e
subaruice <tohaermakov11111@gmail.com>
2025-05-26 22:49:16 +0300

    create base structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/header/Header";
+
+function App() {
+    return (
+        <div className="">
+            <Header/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+       <div className="h-15 bg-[#2B2D41] flex justify-between ">
+           
+       </div>
+    );
+};
+
+export default Header ;
