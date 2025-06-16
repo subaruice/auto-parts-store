5cd29507837ec161412a876b766d20627e3a2f3c
subaruice <tohaermakov11111@gmail.com>
2025-06-16 14:10:34 +0300

    add somy styles

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -9,12 +9,15 @@ const ProductList: React.FC<ItemProps> = ({ items }) => {
     return (
         <div className="flex gap-3 justify-center flex-wrap">
             {items.map(({name, in_stock, Price, product_code}) => 
-                <div className="w-[300px]  border-[0.5px] rounded-[5px] border-black/30 h-auto flex flex-col ">
+                <div className="p-3 w-[300px] border-[0.5px] rounded-[10px] border-black/30 h-auto flex flex-col ">
                     <h3 className="font-medium text-black/90">{name}</h3>
                     <div className="text-[14px]">Код товара: {product_code}</div>
-                    <div className="flex justify-between">
+                    <div className="flex flex-1 justify-between">
                         <div className={`${in_stock === 1 ? 'text-green-700' : 'text-red-800'}`}>{in_stock === 1 ? 'Есть в наличии' : 'Нет в наличии'}</div>
                         <div className="text-[20px]">{Price} ₴</div>
+                    </div>
+                    <div className="flex justify-center items-center">
+                        <button className="py-3 rounded-[7px] w-full text-white font-medium bg-red-700">В корзину</button>
                     </div>
                     <div></div>
                 </div>
