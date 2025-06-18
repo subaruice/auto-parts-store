04eab68c9edd41f839b5426a9d955c1f0dccf53d
subaruice <tohaermakov11111@gmail.com>
2025-06-18 23:51:36 +0300

    add styles, connect to db with subs

--- src/components/sibebar/Sidebar.tsx
+++ src/components/sibebar/Sidebar.tsx
@@ -2,28 +2,68 @@ import Logo from "../../icons/logo-head.png";
 import Home from "../../icons/home.svg?react";
 import Call from "../../icons/call.svg?react";
 import About from "../../icons/about.svg?react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface CategoriesProp {
     categories: any;
 }
 
 const Sidebar: React.FC<CategoriesProp> = ({ categories }) => {
+    const [activeCategoryId, setActiveCategoryId] = useState<number | null>();
+
+    const toggleSub = (id: number) => {
+        setActiveCategoryId((prev) => (prev === id ? null : id));
+    };
+
     return (
-        <div className="flex flex-col px-2 py-4 bg-[#2B2D41] min-w-[320px]">
-            <div className="flex px-8 mb-5 justify-center items-center">
+        <div className="flex flex-col pl-2 py-4 bg-[#2B2D41] min-w-[320px]">
+            <div className="flex px-8 mb-3 justify-center items-center">
                 <img src={Logo} className="w-[90%]" alt="" />
             </div>
             <div className="flex">
-                <div className="flex mt-10 self-center gap-5 flex-col">
+                <div className="flex mt-15 self-start gap-5 flex-col">
                     <Home />
                     <Call />
                     <About />
                 </div>
-                <div className="flex items-center w-full flex-col">
-                    <p className="text-[30px] text-[#dfdfdf]">Каталог</p>
-                    {categories.map((cat:any) => (
-                        <div>{cat.name}</div>
-                    ))}
+                <div className="w-full   pl-4">
+                    {/* <p className="text-[30px] w-[100%] text-center pb-2 text-gray-200">
+                            Каталог
+                        </p> */}
+                    <div className="flex items-center bg-[#202231] rounded-l-[10px] w-full flex-col">
+                        <div className="pr-2 pl-4 tracking-wider text-gray-300 text-[18px] flex flex-col items-start w-full">
+                            {categories.map((cat: any) => (
+                                <div
+                                    key={cat.categoryID}
+                                    className={`${activeCategoryId === cat.categoryID &&
+                                        cat.subcategories.length > 0 &&
+                                        "text-white"} hover:text-white cursor-pointer w-[90%] border-b py-4 border-[#2B2D41]`}
+                                >
+                                    <div onClick={() => toggleSub(cat.categoryID)} className="">
+                                        {cat.name}
+                                    </div>
+                                    <AnimatePresence>
+                                        {activeCategoryId === cat.categoryID && cat.subcategories.length > 0 && (
+                                            <motion.div
+                                                className="ml-4 flex flex-col gap-2 text-[16px] mt-2 text-gray-300"
+                                                initial={{ height: 0, opacity: 0 }}
+                                                animate={{ height: "auto", opacity: 1 }}
+                                                exit={{ height: 0, opacity: 0 }}
+                                                transition={{ duration: 0.3, ease: "easeInOut" }}
+                                            >
+                                                {cat.subcategories.map((sub: any, i: number) => (
+                                                    <div key={i} className="hover:text-white cursor-pointer">
+                                                        {sub.name}
+                                                    </div>
+                                                ))}
+                                            </motion.div>
+                                        )}
+                                    </AnimatePresence>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
