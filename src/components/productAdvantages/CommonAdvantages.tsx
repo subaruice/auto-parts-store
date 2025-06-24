a86a90789190ded7acf93dcd4b37f4cc75125307
subaruice <tohaermakov11111@gmail.com>
2025-06-25 00:10:52 +0300

    create common advantage div copmponent

---
+++ src/components/productAdvantages/CommonAdvantages.tsx
@@ -0,0 +1,35 @@
+const CommonAdvantages = () => {
+    return (
+        <div className="w-full flex gap-3 mt-2">
+            <div className="flex-1/3">
+                <div className="flex gap-2">
+                    <img className="self-start w-max-[68px] object-contain" src="/common-adventure-1.png" alt="" />
+                    <p>
+                        <span className="font-medium text-[16px]">Чешская традиция качества <br/></span>26+ лет на рынке,
+                        производство только из прочных материалов
+                    </p>
+                </div>
+            </div>
+            <div className="flex-1/3">
+                <div className="flex gap-2">
+                    <img className="self-start w-max-[68px] object-contain" src="/common-adventure-2.png" alt="" />
+                    <p>
+                        <span className="font-medium text-[16px]">Огромный асортимент Skoda <br/></span>
+                        Собственное производство автоаксессуаров для автомобилей Škoda.
+                    </p>
+                </div>
+            </div>
+            <div className="flex-1/3">
+                <div className="flex gap-2">
+                    <img className="self-start w-max-[68px] object-contain" src="/common-adventure-3.png" alt="" />
+                    <p>
+                        <span className="font-medium text-[16px]">ЕС Сертификаты <br/></span>
+                        Вся продукция соответствует нормам и директивам ЕС.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CommonAdvantages;
