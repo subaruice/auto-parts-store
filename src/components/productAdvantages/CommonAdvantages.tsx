626ce2f290431002ba2aaaafbd47f6781f037bd2
subaruice <tohaermakov11111@gmail.com>
2025-06-26 16:50:44 +0300

    fix build

--- src/components/productAdvantages/CommonAdvantages.tsx
+++ src/components/productAdvantages/CommonAdvantages.tsx
@@ -1,7 +1,7 @@
 const CommonAdvantages = () => {
     return (
-        <div className="w-full flex gap-3 mt-2">
-            <div className="flex-1/3">
+        <div className="w-full flex gap-3 mt-5">
+            <div className="flex-1/3 pr-3">
                 <div className="flex gap-2">
                     <img className="self-start w-max-[68px] object-contain" src="/common-adventure-1.png" alt="" />
                     <p>
@@ -10,7 +10,7 @@ const CommonAdvantages = () => {
                     </p>
                 </div>
             </div>
-            <div className="flex-1/3">
+            <div className="flex-1/3 pr-3">
                 <div className="flex gap-2">
                     <img className="self-start w-max-[68px] object-contain" src="/common-adventure-2.png" alt="" />
                     <p>
