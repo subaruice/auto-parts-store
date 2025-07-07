e8d6898def748a3741a87402bb68c726c2f4d69d
subaruice <tohaermakov11111@gmail.com>
2025-07-07 22:29:41 +0300

    set the settings for gitlab

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
+    <HashRouter>
         <StrictMode>
             <App />
         </StrictMode>
-    </BrowserRouter>
+    </HashRouter>
 );
