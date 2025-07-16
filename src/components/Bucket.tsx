944fee4f29534b690e605198930e5e6d2d3219f5
subaruice <tohaermakov11111@gmail.com>
2025-07-16 14:44:17 +0300

    v1.2

--- src/components/Bucket.tsx
+++ src/components/Bucket.tsx
@@ -1,38 +1,78 @@
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
+import { X } from "lucide-react";
+import { Link } from "react-router";
 
-const Bucket = () => {
-    const products = JSON.parse(localStorage.getItem("products") ?? "[]");
+const Bucket = memo(() => {
+    const [products, setProducts] = useState(JSON.parse(localStorage.getItem("products") ?? "[]"));
     const [finalPrice, setFinalPrice] = useState(0);
 
     const sumUpPrices = () => {
-        const total = products.reduce((acc:any, product:any) => acc + product.Price, 0)
-        setFinalPrice(total)        
+        const total = products.reduce(
+            (acc: number, product: any) =>
+                product.quantity == 1 ? acc + product.Price : acc + product.Price * product.quantity,
+            0
+        );
+        setFinalPrice(total);
+    };
+
+    const incQuantity = (id: string) => {
+        const updated = products.map((p: any) => (p.productID === id ? { ...p, quantity: p.quantity + 1 } : p));
+        setProducts([...updated]);
+        localStorage.setItem("products", JSON.stringify(updated));
+    };
+
+    const decQuantity = (id: string) => {
+        const updated = products.map((p: any) =>
+            p.productID === id ? { ...p, quantity: p.quantity === 1 ? 1 : p.quantity - 1 } : p
+        );
+        localStorage.setItem("products", JSON.stringify(updated));
+        setProducts([...updated]);
+    };
+
+    const clearProducts = (id:string) => {
+        const updated = products.filter((p:any) => p.productID !== id)
+        localStorage.setItem('products', JSON.stringify(updated))
+        setProducts([...updated]);
     }
+
     useEffect(() => {
         sumUpPrices();
-    },[])
+    }, [products]);
     return (
         <div className="p-4 w-full h-full">
             <div className="rounded-[10px] p-4 bg-white w-full h-full">
                 <div className="flex flex-col gap-4">
                     {products.map((product: any) => (
-                        <div className="flex items-center">
+                        <div key={product.productID} className="flex items-center">
                             <div className="h-20 w-30 mr-4">
                                 <img
                                     className="h-20 object-contain"
-                                    src={`http://milotec.com.ua/pictures/${product.pictures[0].thumbnail ||
-                                        product.pictures[0].filename ||
-                                        product.pictures[0].enlarged}`}
+                                    src={`http://milotec.com.ua/pictures/${product.pictures[0]?.thumbnail ||
+                                        product.pictures[0]?.filename ||
+                                        product.pictures[0]?.enlarged}`}
                                     alt="no picture"
                                 />
                             </div>
-                            <div className="flex-1 pr-4 font-medium text-black/80 text-[17px]">{product.name}</div>
+                            <Link to={`/category/${product.categoryID}/products/${product.productID}`} className="flex-1 pr-4 font-medium text-black/80 text-[17px]">{product.name}</Link>
                             <div className="flex gap-2 w-25">
-                                <button className="text-[26px] text-black/50">-</button>
-                                <p className="border border-black/30 rounded-md text-black/70 py-2 px-5">{0}</p>
-                                <button className="text-[26px] text-black/50">+</button>
+                                <button
+                                    onClick={() => decQuantity(product.productID)}
+                                    className="text-[26px] hover:text-black/65 text-black/50"
+                                >
+                                    -
+                                </button>
+                                <p className="border border-black/30 rounded-md text-black/70 py-2 px-5">
+                                    {product.quantity}
+                                </p>
+                                <button
+                                    onClick={() => incQuantity(product.productID)}
+                                    className="text-[26px] hover:text-black/65 text-black/50"
+                                >
+                                    +
+                                </button>
                             </div>
                             <div className="w-25 text-center text-[18px] ">{product.Price}₴</div>
+                            <X onClick={() => clearProducts(product.productID)} className="cursor-pointer hover:stroke-red-700" color="#fd3535" />
                         </div>
                     ))}
                 </div>
@@ -47,6 +87,6 @@ const Bucket = () => {
             </div>
         </div>
     );
-};
+});
 
 export default Bucket;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -11,32 +11,42 @@ const ProductList = memo(() => {
     const {filteredItems} = useOutletContext<ItemProps>()
     console.log(filteredItems);
     
+    const addProduct = (product:Item, e:any) => {
+        e.stopPropagation()
+        const stored = JSON.parse(localStorage.getItem('products') ?? '[]')
+        const updated = stored.map((p:Item) => 
+            p.productID === product.productID ? {...p, quantity: p.quantity + 1} : {...p, quantity: 1}     
+        )
+        localStorage.setItem('products', JSON.stringify(updated))
+    }
+
     return (
         <div className="flex gap-4 px-4 justify-center flex-wrap ">
-            {filteredItems && filteredItems.map(({ name, in_stock, Price, categoryID, product_code, pictures, brief_description, productID }) => (
-                <Link to={`/category/${categoryID}/products/${productID}`} key={productID} className="p-3 shadow-sm hover:shadow-xl shadow-black/30 bg-white w-[320px] border-[0.5px] border-black/30 h-auto flex flex-col ">
+            {filteredItems && filteredItems.map((p) => (
+                <Link to={`/category/${p.categoryID}/products/${p.productID}`} key={p.productID} className="p-3 shadow-sm hover:shadow-xl shadow-black/30 bg-white w-[320px] border-[0.5px] border-black/30 h-auto flex flex-col ">
                     <div className="">
                         <img
-                            src={`http://milotec.com.ua/pictures/${pictures[1]?.enlarged ||
-                                pictures[1]?.thumbnail ||
-                                pictures[1]?.filename ||
-                                pictures[0]?.enlarged || pictures[0]?.thumbnail || pictures[0]?.filename}`}
+                            src={`http://milotec.com.ua/pictures/${p.pictures[1]?.enlarged ||
+                                p.pictures[1]?.thumbnail ||
+                                p.pictures[1]?.filename ||
+                                p.pictures[0]?.enlarged || p.pictures[0]?.thumbnail || p.pictures[0]?.filename}`}
                             alt="No picture"
                             className="w-full"
                         />
                     </div>
-                    <h3 className="font-medium flex-1 text-[18px] my-2 text-black/90">{name}</h3>
-                    <div className="text-[16px] text-black/70 mb-2" dangerouslySetInnerHTML={{ __html: brief_description }}></div>
-                    <div className="text-[14px] text-black/60">Код товара: {product_code}</div>
+                    <h3 className="font-medium flex-1 text-[18px] my-2 text-black/90">{p.name}</h3>
+                    <div className="text-[16px] text-black/70 mb-2" dangerouslySetInnerHTML={{ __html: p.brief_description }}></div>
+                    <div className="text-[14px] text-black/60">Код товара: {p.product_code}</div>
                     <div className="flex justify-between mb-2 items-center">
-                        <div className={`${in_stock === 1 ? "text-[#198754]" : "text-rose-800"}`}>
-                            {in_stock === 1 ? "Есть в наличии" : "Нет в наличии"}
+                        <div className={`${p.in_stock === 1 ? "text-[#198754]" : "text-rose-800"}`}>
+                            {p.in_stock === 1 ? "Есть в наличии" : "Нет в наличии"}
                         </div>
-                        <div className="text-[22px]">{Price} ₴</div>
+                        <div className="text-[22px]">{p.Price} ₴</div>
                     </div>
+                    
                     <div className="flex cursor-pointer justify-center gap-2 items-center hover:bg-[#468153] transition-all bg-[#3fa357] ">
                         <Bucket className="w-7" />
-                        <button className="py-3 text-[18px]  text-white font-medium ">В корзину</button>
+                        <button onClick={(e) => addProduct(p, e)} className="py-3 text-[18px]  text-white font-medium ">В корзину</button>
                     </div>
 
                 </Link>
