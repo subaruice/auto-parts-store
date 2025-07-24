864abc33d69d1ab0f5270c719d400ab785013cf4
subaruice <tohaermakov11111@gmail.com>
2025-07-24 10:47:53 +0300

    change images in slider

--- src/components/UI/SliderHomepage.tsx
+++ src/components/UI/SliderHomepage.tsx
@@ -2,12 +2,12 @@ import { useState, memo, useRef, useEffect } from "react";
 import { Link } from "react-router";
 
 const imageSources = [
-    { src: "Obr-1.png", categoryID: "148" },
-    { src: "Obr-2.png", categoryID: "158" },
-    { src: "Obr-3.png", categoryID: "162" },
-    { src: "Obr-4.png", categoryID: "162" },
-    { src: "Obr-5.png", categoryID: "154" },
-    { src: "Obr-6.png", categoryID: "154" },
+    { src: "sliderIMG/slider1.png", categoryID: "148" },
+    { src: "sliderIMG/slider2.png", categoryID: "158" },
+    { src: "sliderIMG/slider3.png", categoryID: "162" },
+    { src: "sliderIMG/slider4.png", categoryID: "162" },
+    { src: "sliderIMG/slider5.png", categoryID: "154" },
+    { src: "sliderIMG/slider6.png", categoryID: "154" },
 ];
 
 const SliderHomepage = memo(() => {
@@ -41,7 +41,7 @@ const SliderHomepage = memo(() => {
     return (
         <div className="gap-3 flex flex-col justify-center">
             <Link className="flex justify-center" to={`/category/${current.categoryID}`}>
-                <img src={`https://www.milotec.net/fotky72500/slider/${current.src}`} alt="no image" />
+                <img src={current.src} alt="no image" />
             </Link>
             <div className="flex gap-2 justify-center">
                 {imageSources.map((_, i) => (
