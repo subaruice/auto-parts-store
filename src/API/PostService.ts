207e3e652282b71ee6fda0f0db29cf41af06ef9c
subaruice <tohaermakov11111@gmail.com>
2025-07-27 22:35:05 +0300

    fix build

--- src/API/PostService.ts
+++ src/API/PostService.ts
@@ -11,11 +11,11 @@ export default class PostService {
         return response;
     }
     static async getAllCategories() {
-        const response = await axios.get("http://localhost:3001/api/category/");
+        const response = await axios.get("https://backend-auto-7uaq.onrender.com/api/category/");
         return response;
     }
     static async getCategoryByProduct(id: string, limit?: number, offset?: number) {
-        const response = await axios.get(`http://localhost:3001/api/products/category/${id}`, {
+        const response = await axios.get(`https://backend-auto-7uaq.onrender.com/api/products/category/${id}`, {
             params: {
                 limit: limit || 20,
                 offset: offset || 0,
@@ -24,12 +24,12 @@ export default class PostService {
         return response;
     }
     static async getProductByID(id: string) {
-        const response = await axios.get("http://localhost:3001/api/products/product/" + id);
+        const response = await axios.get("https://backend-auto-7uaq.onrender.com/api/products/product/" + id);
         return response;
     }
 
     static async getAllSaleProducts(limit?: number, offset?: number) {
-        const response = await axios.get("http://localhost:3001/api/products/", {
+        const response = await axios.get("https://backend-auto-7uaq.onrender.com/api/products/", {
             params: {
                 limit: limit || 20,
                 offset: offset || 0,
