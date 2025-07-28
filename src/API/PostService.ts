a76cbd57e72f0b2e85867382b6beb1d89c321a5a
subaruice <tohaermakov11111@gmail.com>
2025-07-28 08:19:18 +0300

    fix build

--- src/API/PostService.ts
+++ src/API/PostService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class PostService {
     static async getAllProducts(limit?: number, offset?: number) {
-        const response = await axios.get("http://localhost:3001/api/prodicts/catalog/", {
+        const response = await axios.get("https://backend-auto-production.up.railway.app/api/prodicts/catalog/", {
             params: {
                 limit: limit || 20,
                 offset: offset || 0,
@@ -11,11 +11,11 @@ export default class PostService {
         return response;
     }
     static async getAllCategories() {
-        const response = await axios.get("https://backend-auto-7uaq.onrender.com/api/category/");
+        const response = await axios.get("https://backend-auto-production.up.railway.app/api/category/");
         return response;
     }
     static async getCategoryByProduct(id: string, limit?: number, offset?: number) {
-        const response = await axios.get(`https://backend-auto-7uaq.onrender.com/api/products/category/${id}`, {
+        const response = await axios.get(`https://backend-auto-production.up.railway.app/api/products/category/${id}`, {
             params: {
                 limit: limit || 20,
                 offset: offset || 0,
@@ -24,12 +24,12 @@ export default class PostService {
         return response;
     }
     static async getProductByID(id: string) {
-        const response = await axios.get("https://backend-auto-7uaq.onrender.com/api/products/product/" + id);
+        const response = await axios.get("https://backend-auto-production.up.railway.app/api/products/product/" + id);
         return response;
     }
 
     static async getAllSaleProducts(limit?: number, offset?: number) {
-        const response = await axios.get("https://backend-auto-7uaq.onrender.com/api/products/", {
+        const response = await axios.get("https://backend-auto-production.up.railway.app/api/products/", {
             params: {
                 limit: limit || 20,
                 offset: offset || 0,
