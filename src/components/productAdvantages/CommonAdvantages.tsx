ab5129a17b3962f31d409bede6bf4f2d455feae0
subaruice <tohaermakov11111@gmail.com>
2025-07-29 06:48:37 +0300

    fix build

--- src/components/productAdvantages/CommonAdvantages.tsx
+++ src/components/productAdvantages/CommonAdvantages.tsx
@@ -1,9 +1,9 @@
 const CommonAdvantages = () => {
     return (
-        <div className="w-full flex gap-3 mt-5 text-black/80">
+        <div className="w-full flex-col flex lg:flex-row gap-3 mt-5 text-black/80">
             <div className="flex-1/3 pr-3">
                 <div className="flex gap-2">
-                    <img className="self-start w-max-[68px] object-contain" src="/auto-parts-store/common-adventure-1.png" alt="" />
+                    <img className="self-start w-[51px] h-[51px] object-contain" src="/auto-parts-store/common-adventure-1.png" alt="" />
                     <p>
                         <span className="font-medium text-[16px]">Чешская традиция качества <br/></span>26+ лет на рынке,
                         производство только из прочных материалов
@@ -12,7 +12,7 @@ const CommonAdvantages = () => {
             </div>
             <div className="flex-1/3 pr-3">
                 <div className="flex gap-2">
-                    <img className="self-start w-max-[68px] object-contain" src="/auto-parts-store//common-adventure-2.png" alt="" />
+                    <img className="self-start w-[51px] h-[51px] object-contain" src="/auto-parts-store/common-adventure-2.png" alt="" />
                     <p>
                         <span className="font-medium text-[16px]">Огромный асортимент Skoda <br/></span>
                         Собственное производство автоаксессуаров для автомобилей Škoda.
@@ -21,7 +21,7 @@ const CommonAdvantages = () => {
             </div>
             <div className="flex-1/3">
                 <div className="flex gap-2">
-                    <img className="self-start w-max-[68px] object-contain" src="/auto-parts-store/common-adventure-3.png" alt="" />
+                    <img className="self-start w-[51px] h-[51px] object-contain" src="/auto-parts-store/common-adventure-3.png" alt="" />
                     <p>
                         <span className="font-medium text-[16px]">ЕС Сертификаты <br/></span>
                         Вся продукция соответствует нормам и директивам ЕС.
--- src/components/sibebar/SidebarMobile.tsx
+++ src/components/sibebar/SidebarMobile.tsx
@@ -10,7 +10,7 @@ import { memo } from "react";
 interface CategoriesProp {
     categories: any;
     categoryID?: string | undefined;
-    setSidebar: React.Dispatch<React.SetStateAction<boolean>>
+    setSidebar: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const SidebarMobile: React.FC<CategoriesProp> = memo(({ categories, categoryID, setSidebar }) => {
@@ -56,7 +56,7 @@ const SidebarMobile: React.FC<CategoriesProp> = memo(({ categories, categoryID, 
     }, [categoryID, categories]);
 
     return (
-        <div onClick={e => e.stopPropagation()} className="grow h-full flex flex-col  shrink-0 w-[90%]">
+        <div onClick={(e) => e.stopPropagation()} className="grow h-full flex flex-col  shrink-0 w-[90%]">
             <div className="flex grow text-gray-200 text-[22px] bg-[#2f3247] flex-col">
                 {/* <div className="flex mt-15 self-start gap-5 flex-col">
                     <Link onClick={clearStates} className="cursor-pointer" to={"/"}>
@@ -71,27 +71,37 @@ const SidebarMobile: React.FC<CategoriesProp> = memo(({ categories, categoryID, 
                 </div> */}
                 <div className="border-b border-gray-700 pl-4 pr-2 py-7">Главная</div>
                 <div className="border-b border-gray-700 pr-2">
-                    <div className={`${isCatalogOpen && 'text-white'} indent-4 py-7`} onClick={() => setIsCatalogOpen(!isCatalogOpen)}>Каталог</div>
+                    <div
+                        className={`${isCatalogOpen && "text-white"} indent-4 py-7`}
+                        onClick={() => setIsCatalogOpen(!isCatalogOpen)}
+                    >
+                        Каталог
+                    </div>
+                    <AnimatePresence mode="wait">
                     {isCatalogOpen && (
-                        <div className="w-full pl-2">
-                            <div className="flex items-center bg-[#202231] w-full flex-col">
-                                <div className="pr-2 pl-4 tracking-wider text-gray-300 text-[18px] flex flex-col items-start w-full">
-                                    {categories.map((cat: any) => (
-                                        <div
-                                            
-                                            key={cat.categoryID}
-                                            className={`${
-                                                activeCategoryId === cat.categoryID && "text-white"
-                                            } hover:text-white cursor-pointer w-[90%] border-b py-4 border-[#2B2D41]`}
-                                        >
-                                            <Link
-                                                to={`/category/${cat.categoryID}`}
-                                                onClick={() => toggleSub(cat.categoryID)}
-                                                className="w-full"
+                            <motion.div 
+                            initial={{scaleY: 0, transformOrigin: 'top'}}
+                            animate={{scaleY: 1}}
+                            exit={{scaleY: 0}}
+                            transition={{duration: 0.2}}
+                            className="w-full pl-2">
+                                <div className="flex items-center bg-[#202231] w-full flex-col">
+                                    <div className="pr-2 pl-4 tracking-wider text-gray-300 text-[18px] flex flex-col items-start w-full">
+                                        {categories.map((cat: any) => (
+                                            <div
+                                                key={cat.categoryID}
+                                                className={`${
+                                                    activeCategoryId === cat.categoryID && "text-white"
+                                                } hover:text-white cursor-pointer w-[90%] border-b py-4 border-[#2B2D41]`}
                                             >
-                                                {cat.name}
-                                            </Link>
-                                            <AnimatePresence>
+                                                <Link
+                                                    to={`/category/${cat.categoryID}`}
+                                                    onClick={() => toggleSub(cat.categoryID)}
+                                                    className="w-full"
+                                                >
+                                                    {cat.name}
+                                                </Link>
+
                                                 {activeCategoryId === cat.categoryID &&
                                                     cat.subcategories.length > 0 && (
                                                         <motion.div
@@ -119,13 +129,13 @@ const SidebarMobile: React.FC<CategoriesProp> = memo(({ categories, categoryID, 
                                                             ))}
                                                         </motion.div>
                                                     )}
-                                            </AnimatePresence>
-                                        </div>
-                                    ))}
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
+                            </motion.div>
                     )}
+                    </AnimatePresence>
                 </div>
                 <div className="border-b border-gray-700 pl-4 pr-2 py-7">Контакты</div>
                 <div className="border-b border-gray-700 pl-4 pr-2 py-7">О нас</div>
